fe25c8810ec67c6b811c23bf860d06e7dcc8f419
HyeonjongPark <guswhd9844@gmail.com>
2020-12-21 14:19:49 +0900

    property analysis

--- src/006_real_estate_tm.R
+++ src/006_real_estate_tm.R
@@ -1,7 +1,7 @@
 
 rm(list = ls())
 
-
+library(tibble)
 library(readr)
 library(rlang)
 library(dplyr)
@@ -10,61 +10,39 @@ library(data.table)
 library(stringr)
 
 getwd()
-chat_raw = read_lines("./data/06_real_estate_talk.txt")
-chat_raw = as.data.frame(chat_raw)
-chat_raw %>% head
 
-colnames(chat_raw)[1] = "text"
-chat_raw$text = as.character(chat_raw$text)
-chat_raw %>% head
+property_2020 = fread("./data/data006/newstapa-jaesan-2020/정기공개/CSV/newstapa-jaesan-2020-records.csv", encoding = "UTF-8")
+property_2019 = fread("./data/data006/newstapa-jaesan-2019/CSV/newstapa-jaesan-2019-records.csv")
+property_2018 = fread("./data/data006/newstapa-jaesan-2018/CSV/newstapa-jaesan-2018-records.csv")
 
-chat_raw = chat_raw %>% filter(substr(text, 1, 4) == "2020" | substr(text, 1, 4) == "2019" | substr(text, 1, 4) == "2018" | substr(text, 1, 4) == "2017") 
-chat_raw$ind = NA
-# test
-chat_raw$text = iconv(chat_raw$text , 'UTF-8') 
-chat_raw_2 = chat_raw %>% filter(!is.na(text))
-
-chat_raw_2 %>% head
-chat_raw_2 %>% tail
-
-chat_raw_2$ind = 1:nrow(chat_raw_2)
-rownames(chat_raw_2) = 1:nrow(chat_raw_2)
+property_2020 = property_2020[,c("연도", "이름", "소속", "직위", "본인과의 관계", "재산 대분류", "재산의 종류", "소재지 면적 등 권리의 명세", "현재가액")]
+property_2019 = property_2019[,c("연도", "이름", "소속", "직위", "본인과의 관계", "재산 대분류", "재산의 종류", "소재지 면적 등 권리의 명세", "현재가액")]
+property_2018 = property_2018[,c("연도", "이름", "소속", "직위", "본인과의 관계", "재산 대분류", "재산의 종류", "소재지 면적 등 권리의 명세", "현재가액")]
 
 
+property = rbind(property_2018, property_2019)
+colnames(property) = c("year", "name", "belong", "spot", "relationship",
+                       "category", "kinds", "detail", "price")
 
 
-chat_1 = chat_raw_2
-chat_1 = chat_1[!grepl("http", chat_1$text),]
+property$`재산 대분류` %>% unique
+
+property_toji = property %>% filter(category == "토지")
+property_toji %>% head(100)
+property_toji %>% tail
+property_toji = property_toji[grepl("번지",property_toji$detail),]
+
+property_toji$adress = regmatches(property_toji$detail, regexpr(".+[번]+[지]", property_toji$detail))
+property_toji$adress = enc2utf8(property_toji$adress)
+property_toji$adress = as.character(property_toji$adress)
 
 
-#chat_1 = chat_1 %>% head(100)
-chat_1$text = gsub('[ㄱ-ㅎ]','',chat_1$text)
-chat_1$text = gsub('[ㅏ-ㅣ]','',chat_1$text)
-chat_1$text = gsub('"','',chat_1$text)
+property_toji %>% head
 
-chat_1 %>% head
+register_google("AIzaSyBhkb5Pp34iyZIaOtEZp4HSPEH2egDxYts")
+lng_lat_500 = geocode(property_toji$adress[1:500])
 
-str_extract(chat_1$text, ", .")
-regmatches(chat_1$text, regexpr("([, ].+[.][ : ])", chat_1$text)) %>% head
+register_google("AIzaSyD_BbZHN4VvQ8nW-hX-ynYBjH-UlShQMRE")
+lng_lat_1000 = geocode(property_toji$adress[501:1000])
 
 
-ind_real = chat_1$ind[grepl("[0-9]+. [0-9]+. [0-9]+. [가-힣]+. [0-9]+:[0-9]+.", chat_1$text)]
-ind_real %>% length
-ind_real %>% tail
-date = str_extract(chat_1$text[chat_1$ind %in% ind_real], "[0-9]+. [0-9]+. [0-9]+. [가-힣]+. [0-9]+:[0-9]+.")
-speaker = gsub(" ","",substr(gsub("[0-9]+. [0-9]+. [0-9]+. [가-힣]+. [0-9]+:[0-9]+.", "", chat_1$text[chat_1$ind %in% ind_real]),1,4)) 
-speaker %>% length
-text = gsub("[0-9]+. [0-9]+. [0-9]+. [가-힣]+. [0-9]+:[0-9]+., [가-힣][가-힣][가-힣] : ", "", chat_1$text[chat_1$ind %in% ind_real]) 
-text %>% length
-
-chat2 = cbind(ind_real, date, speaker, text) %>% as.data.frame() 
-colnames(chat2)[1] = "ind"
-
-
-
-chat_1$text[chat_1$text == "삭제된 메시지입니다."] = NA
-chat_1$text[chat_1$text == "이모티콘"] = ""
-chat_1$text[chat_1$text == "사진"] = NA
-chat_1$text[chat_1$text == "동영상"] = NA
-
-chat_1$text <- gsub('[~!@#$%^&*()_+=?^]<>','',chat_1$text)
